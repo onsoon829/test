8ee9e960e7d1c4390364c97931c5d7fef7770b73
onsoon829 <150321906+onsoon829@users.noreply.github.com>
2024-04-24 19:48:52 +0900

    Add files via upload

--- src/main/java/com/blogfriday/cart/controller/CartController.java
+++ src/main/java/com/blogfriday/cart/controller/CartController.java
@@ -3,11 +3,14 @@ package com.blogfriday.cart.controller;
 import com.blogfriday.cart.dto.CartDTO;
 import com.blogfriday.cart.dto.CartItemDTO;
 import com.blogfriday.cart.service.CartService;
-
+import com.blogfriday.product.dto.ProductDTO;
+import com.blogfriday.product.dto.ProductimgDTO;
+import com.blogfriday.product.service.ProductService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import jnr.ffi.Struct.int16_t;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.Charset;
@@ -42,98 +45,137 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin("*")
 public class CartController {
 
-	
-    private final CartService cartService;
-	
-	@Autowired
-	public CartController(CartService cartService) {
-		this.cartService = cartService;
-	}
+   
+   private final CartService cartService;
+   
+   @Autowired
+   private ProductService productService;
+   
 
-	@GetMapping("/list")//리스트 뽑아오기
-	public ResponseEntity<List<CartDTO>> getAllCarts(){
-		return ResponseEntity.ok(cartService.getAllCarts());
-	}
+   public CartController(CartService cartService) {
+      this.cartService = cartService;
+   }
 
-//	@GetMapping("/list/{user-id}")//리스트 뽑아오기
-//	public ResponseEntity<List<CartItemDTO>> getCartsByUserId(@PathVariable("user_id") int user_id){
-//		return ResponseEntity.ok(cartService.getCartsByUserId(user_id));
-//	}
-	
-	@GetMapping("/{cart_product_code}")
-	public ResponseEntity<CartDTO>getCartById(@PathVariable("cartProductCode")int cartProductCode){
-		
-		CartDTO cart = cartService.getCartByCode(cartProductCode);
-		if(cart == null) {
-			return ResponseEntity.notFound().build();
-		}
-		return ResponseEntity.ok(cart);
-	}
-	
-	
-	 @GetMapping("/list/{user_id}") // 유저 ID를 기반으로 장바구니 목록 검색
-	    public ResponseEntity<Map<String, Object>> searchCartList(@PathVariable("user_id") int user_id) {
-	        Map<String, Object> map = new HashMap<>();
-	        
-	        List<CartItemDTO> cartList = cartService.searchCartList(user_id);
-	        
-	        if (cartList.isEmpty()) {
-	            
-	            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
-	        } else {
-	            
-	            map.put("cartList", cartList);
-	            return ResponseEntity.ok(map);
-	        }
-	    }
+   @GetMapping("/list")//리스트 뽑아오기
+   public ResponseEntity<List<CartDTO>> getAllCarts(){
+      return ResponseEntity.ok(cartService.getAllCarts());
+   }
+
+//   @GetMapping("/list/{user-id}")//리스트 뽑아오기
+//   public ResponseEntity<List<CartItemDTO>> getCartsByUserId(@PathVariable("user_id") int user_id){
+//      return ResponseEntity.ok(cartService.getCartsByUserId(user_id));
+//   }
+   
+   @GetMapping("/{cart_product_code}")
+   public ResponseEntity<CartDTO>getCartById(@PathVariable("cartProductCode")int cartProductCode){
+      
+      CartDTO cart = cartService.getCartByCode(cartProductCode);
+      if(cart == null) {
+         return ResponseEntity.notFound().build();
+      }
+      return ResponseEntity.ok(cart);
+   }
+   
+   
+    @GetMapping("/list/{user_id}") // 유저 ID를 기반으로 장바구니 목록 검색
+       public ResponseEntity<Map<String, Object>> searchCartList(@PathVariable("user_id") int user_id) {
+           Map<String, Object> map = new HashMap<>();
+           
+           List<CartItemDTO> cartList = cartService.searchCartList(user_id);
+           
+           
+
+           if(!cartList.isEmpty()) {
+              for(CartItemDTO cartItem : cartList) {
+                 ProductimgDTO productImgDTO = productService.contentImgProcess(cartItem.getProduct_code());
+                  cartItem.setProduct_img0(productImgDTO.getProduct_img0());
+                  //cartItem.setProduct_img1(productImgDTO.getProduct_img1());
+              }
+              map.put("cartList", cartList);
+              return ResponseEntity.ok(map);
+           }else {
+              return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
+           }        
+           
+           
+           
+//           if (cartList.isEmpty()) {             
+//               return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
+//           } else {
+//               
+//               map.put("cartList", cartList);
+//               return ResponseEntity.ok(map);
+//           }
+       }
 /////////////////////////////////////////////////////////////////////
-//	@GetMapping("/list/{user_id}")
-//	public ResponseEntity<Map<String, Object>> searchcartlist(@PathVariable("user_id") int user_id){
-//		Map<String, Object>map = new HashMap<>();
-//		log.info("들어온 제품:{}", user_id);
-//		List<CartDTO> cartList = cartService.searchCartList(user_id);
-//	
-//		List<Integer> productCodes = cartList.stream().map(CartDTO::getProduct_code).collect(Collectors.toList());
-//	
-//	Map<Integer, Object>cartImagesResults = new HashMap<>();
-//	for(Integer productCode : productCodes) {
-//		Object imgResult = cartService.contentImgProcess(productCode);
-//		cartImagesResults.put(productCode, imgResult);
-//	}
-//	
-//	map.put("cartList", cartList);
-//	map.put("productImages", cartImagesResults);
-//	
-//	log.info("cartList=get:{}", map.get("cartList"));
-//	
-//	return ResponseEntity.ok(map);
-//	}
-/////////////////////////////////////////////////////////	
-	
-	// 상품 추가
-	@PostMapping("/add")
-	public ResponseEntity<Void> addCart(@RequestBody CartDTO cart) {
-		System.out.println("cart>>" + cart.getUser_id());
-		cartService.insert(cart);
-		return ResponseEntity.ok(null);
-	}
-	
-	//상품 수정
-	@PutMapping("/update/{cart_product_code}")
-	public ResponseEntity<Void>updateCart(@PathVariable("cart_product_code")
-	int cart_product_code, @RequestBody CartDTO cart){
-		cart.setCart_product_code(cart_product_code);
-		cartService.update(cart);
-		return ResponseEntity.ok().build();
-	}
-	
-	//상품 삭제
-	@DeleteMapping("/delete/{cart_product_code}")
-	public ResponseEntity<Void>deleteCart(@PathVariable ("cart_product_code")
-	int cart_product_code){
-		cartService.delete(cart_product_code);
-		return ResponseEntity.ok().build();
-	}
+//   @GetMapping("/list/{user_id}")
+//   public ResponseEntity<Map<String, Object>> searchcartlist(@PathVariable("user_id") int user_id){
+//      Map<String, Object>map = new HashMap<>();
+//      log.info("들어온 제품:{}", user_id);
+//      List<CartDTO> cartList = cartService.searchCartList(user_id);
+//   
+//      List<Integer> productCodes = cartList.stream().map(CartDTO::getProduct_code).collect(Collectors.toList());
+//   
+//   Map<Integer, Object>cartImagesResults = new HashMap<>();
+//   for(Integer productCode : productCodes) {
+//      Object imgResult = cartService.contentImgProcess(productCode);
+//      cartImagesResults.put(productCode, imgResult);
+//   }
+//   
+//   map.put("cartList", cartList);
+//   map.put("productImages", cartImagesResults);
+//   
+//   log.info("cartList=get:{}", map.get("cartList"));
+//   
+//   return ResponseEntity.ok(map);
+//   }
+/////////////////////////////////////////////////////////   
+   
+   // 상품 추가
+   @PostMapping("/add")
+   public ResponseEntity<Void> addCart(@RequestBody CartDTO cart) {
+      System.out.println("cart>>" + cart.getUser_id());
+      cartService.insert(cart);
+      return ResponseEntity.ok(null);
+   }
+   
+   // 상품 수정
+   @PutMapping("/update/{cart_product_code}/{newCount}")
+   public ResponseEntity<Void> updateCart(@PathVariable("cart_product_code") int cart_product_code, @PathVariable("newCount") int newCount) {
+       Map<String, Object> map = new HashMap<>();
+       map.put("cart_product_code", cart_product_code);
+       map.put("newCount", newCount);
+       cartService.update(map);
+       return ResponseEntity.ok().build();
+   }
+
+//   public ResponseEntity<Void> updateCart(@PathVariable("cart_product_code") int cart_product_code, @PathVariable("newCount") int newCount) {
+//       CartDTO cart = new CartDTO();
+//       System.out.println(cart_product_code);
+//       System.out.println(newCount);
+//       cart.setCart_product_code(cart_product_code);
+//       cart.setCart_product_count(newCount);
+//       cartService.update(cart_product_code, newCount);
+//       System.out.println(cart_product_code);
+//       System.out.println(newCount);
+//       return ResponseEntity.ok().build();
+//   }
+   
+//      @PutMapping("/update/{cart_product_code}")
+//      public ResponseEntity<Void>updateCart(@PathVariable("cart_product_code")
+//      int cart_product_code, @RequestBody CartDTO cart){
+//         cart.setCart_product_code(cart_product_code);
+//         cartService.update(cart);
+//         return ResponseEntity.ok().build();
+//      }
+
+   
+   //상품 삭제
+   @DeleteMapping("/delete/{cart_product_code}")
+   public ResponseEntity<Void>deleteCart(@PathVariable ("cart_product_code")
+   int cart_product_code){
+      cartService.delete(cart_product_code);
+      return ResponseEntity.ok().build();
+   }
 
 }//end class
-
